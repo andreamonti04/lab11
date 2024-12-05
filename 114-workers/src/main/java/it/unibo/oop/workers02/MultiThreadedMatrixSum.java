af78261da44c32be10216b475f41b0ffa884f9a2
emme004 <andrea.monti24@studio.unibo.it>
2024-12-05 10:09:11 +0100

    ex 114 completed implementation

---
+++ 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedMatrixSum.java
@@ -0,0 +1,88 @@
+package it.unibo.oop.workers02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is a standard implementation of the calculation.
+ * 
+ */
+public final class MultiThreadedMatrixSum implements SumMatrix {
+
+    private final int nthread;
+
+    /**
+     * 
+     * @param nthread
+     *            no. of thread performing the sum.
+     */
+    public MultiThreadedMatrixSum(final int nthread) {
+        this.nthread = nthread;
+    }
+
+    private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private double res;
+
+        /**
+         * Build a new worker.
+         * 
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final double[][] matrix, final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public void run() {
+            for (int i = startpos; i < matrix.length && i < startpos + nelem; i++) {
+                for (final double elem : this.matrix[i]) {
+                    this.res += elem;
+                }
+            }
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         * 
+         * @return the sum of every element in the array
+         */
+        public double getResult() {
+            return this.res;
+        }
+
+    }
+
+    @Override
+    public double sum(final double[][] matrix) {
+        final int size = matrix.length % nthread + matrix.length / nthread;
+        final List<Worker> workers = new ArrayList<>(nthread);
+        for (int start = 0; start < matrix.length; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        for (final Worker w: workers) {
+            w.start();
+        }
+        double sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        return sum;
+    }
+}
